b189e8ec9a2bcc36b1342c08efb3a584a11d79d5
Bakhayokho Mamadou <mbakhayokho.presta@conservateur.fr>
2021-03-07 22:09:11 +0100

    Modif port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./index');
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log(`Le serveur ecoute sur le port: ${port}`);
+    console.log(process.env.PORT)
 })
